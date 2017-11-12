695728e7174380d051f8cead6b2254242b8efa24
Remco Janssen <entreco@gmail.com>
2017-11-12 09:20:00 +0100

    Changed Displaying of Results

--- android/DartsScorecard/app/src/main/java/nl/entreco/dartsscorecard/SplashViewModel.kt
+++ android/DartsScorecard/app/src/main/java/nl/entreco/dartsscorecard/SplashViewModel.kt
@@ -2,6 +2,7 @@ package nl.entreco.dartsscorecard
 
 import android.databinding.ObservableField
 import nl.entreco.domain.Game
+import nl.entreco.domain.Score
 import nl.entreco.domain.Turn
 import java.util.*
 
@@ -12,18 +13,33 @@ class SplashViewModel {
 
     private val g : Game = Game().apply { start() }
     private val summary: StringBuilder = StringBuilder(g.state).newline()
-    val game : ObservableField<String> = ObservableField(summary.toString())
+    val history: ObservableField<String> = ObservableField(summary.toString())
+    val score: ObservableField<String> = ObservableField()
 
     fun submitRandom(){
         val turn = Turn(rand(), rand(), rand())
         g.handle(turn)
-        summary.append(g.scores).newline().append(g.state).newline()
-        game.set(summary.toString())
+
+        score.set(format(g.scores))
+
+        summary.insert(0, "\n")
+        summary.insert(0, "throw:$turn")
+        summary.insert(0, "\n\n")
+        summary.insert(0, g.state)
+        history.set(summary.toString())
+    }
+
+    private fun format(scores: Array<Score>): String {
+        return StringBuilder().apply {
+            scores.forEach {
+                append(it).newline()
+            }
+        }.toString()
     }
 
     private fun rand(): Int = Random().nextInt(20) * Random().nextInt(3)
 
-    fun StringBuilder.newline() : StringBuilder {
+    private fun StringBuilder.newline() : StringBuilder {
         return append("\n")
     }
 }
--- android/DartsScorecard/domain/src/main/java/nl/entreco/domain/Turn.kt
+++ android/DartsScorecard/domain/src/main/java/nl/entreco/domain/Turn.kt
@@ -1,3 +1,9 @@
 package nl.entreco.domain
 
-data class Turn (val d1: Int, val d2: Int, val d3: Int)
+data class Turn (val d1: Int, val d2: Int, val d3: Int){
+    private val sum = d1 + d2 + d3
+
+    override fun toString(): String {
+        return "$sum ($d1,$d2,$d3)"
+    }
+}
