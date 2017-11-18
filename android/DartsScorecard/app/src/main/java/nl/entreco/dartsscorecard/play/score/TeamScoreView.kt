54232b64a23ca3d9ed36c6264a5fb79a9150b958
Remco Janssen <entreco@gmail.com>
2017-11-18 21:23:04 +0100

    #10: Styling for ScoreView

---
+++ android/DartsScorecard/app/src/main/java/nl/entreco/dartsscorecard/play/score/TeamScoreView.kt
@@ -0,0 +1,14 @@
+package nl.entreco.dartsscorecard.play.score
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.LinearLayout
+
+/**
+ * Created by Entreco on 18/11/2017.
+ */
+class TeamScoreView(context: Context, attrs: AttributeSet, defStyleAttr : Int, defStyleRes : Int)
+    : LinearLayout(context, attrs, defStyleAttr, defStyleRes) {
+
+
+}
