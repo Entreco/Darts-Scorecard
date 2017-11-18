a7184d2b67be7053ffc193404d86f307b26e5fa5
Remco Janssen <entreco@gmail.com>
2017-11-18 14:01:34 +0100

    #9: Added to String for Teams

--- android/DartsScorecard/domain/src/main/java/nl/entreco/domain/play/model/players/Team.kt
+++ android/DartsScorecard/domain/src/main/java/nl/entreco/domain/play/model/players/Team.kt
@@ -3,14 +3,14 @@ package nl.entreco.domain.play.model.players
 /**
  * Created by Entreco on 18/11/2017.
  */
-class Team(vararg val players: Player = emptyArray()){
+class Team(vararg val players: Player = emptyArray()) {
     private var START_TURN = -1
     private var turns = START_TURN
     private var offset = 0
     private var lastLeg = 0
     private var lastSet = 0
 
-    fun next(currentLeg: Int = 0, currentSet: Int = 0) : Player {
+    fun next(currentLeg: Int = 0, currentSet: Int = 0): Player {
         when {
             isNewSet(currentSet) -> newSet()
             isNewLeg(currentLeg) -> newLeg()
@@ -34,5 +34,16 @@ class Team(vararg val players: Player = emptyArray()){
     private fun newLeg() {
         turns = START_TURN
         offset++
+    }
+
+    override fun toString(): String {
+        return StringBuilder(firstOrNo().name).apply {
+            players.drop(1).forEach { append(" & ").append(it.name) }
+        }.toString()
+    }
+
+    private fun firstOrNo(): Player {
+        return if (players.isEmpty()) NoPlayer()
+        else players[0]
     }
 }
