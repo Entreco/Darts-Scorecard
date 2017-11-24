18323aae373a6a27b122a1bad01f90a3cd88b77c
Remco Janssen <entreco@gmail.com>
2017-11-24 13:25:55 +0100

    #3: Styling 180

--- android/DartsScorecard/app/src/main/java/nl/entreco/dartsscorecard/play/input/InputViewModel.kt
+++ android/DartsScorecard/app/src/main/java/nl/entreco/dartsscorecard/play/input/InputViewModel.kt
@@ -32,18 +32,10 @@ open class InputViewModel @Inject constructor(private val analytics: Analytics) 
             else -> {
                 dart3 = rand()
 
-                if(Random().nextBoolean()) {
-                    val turn = Turn(dart1, dart2, dart3)
-                    listener.onDartThrown(dart3)
-                    listener.onTurnSubmitted(turn)
-                    analytics.trackAchievement("scored: $turn")
-                } else {
-                    val turn = Turn(60, 60, 60)
-                    listener.onDartThrown(60)
-                    listener.onTurnSubmitted(turn)
-                    analytics.trackAchievement("scored: $turn")
-                }
-
+                val turn = Turn(dart1, dart2, dart3)
+                listener.onDartThrown(dart3)
+                listener.onTurnSubmitted(turn)
+                analytics.trackAchievement("scored: $turn")
             }
         }
         count++
--- android/DartsScorecard/app/src/main/java/nl/entreco/dartsscorecard/play/score/ScoreBindings.kt
+++ android/DartsScorecard/app/src/main/java/nl/entreco/dartsscorecard/play/score/ScoreBindings.kt
@@ -2,9 +2,10 @@ package nl.entreco.dartsscorecard.play.score
 
 import android.content.Context
 import android.databinding.BindingAdapter
+import android.graphics.Color
+import android.support.annotation.ColorRes
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
-import android.util.TypedValue
 import android.view.animation.AccelerateDecelerateInterpolator
 import android.widget.TextView
 import nl.entreco.dartsscorecard.CounterTextView
@@ -12,10 +13,11 @@ import nl.entreco.dartsscorecard.R
 import nl.entreco.domain.play.model.players.Team
 import nl.entreco.domain.settings.ScoreSettings
 import android.R.attr.data
-import android.support.v4.content.ContextCompat
-import android.content.res.TypedArray
+import android.support.annotation.ColorInt
 import android.content.res.Resources.Theme
-import android.graphics.Color
+import android.util.TypedValue
+
+
 
 
 /**
@@ -46,23 +48,32 @@ class ScoreBindings {
         @BindingAdapter("current")
         fun showCurrent(view: TextView, score: Int) {
             view.text = score.toString()
-            if(score <= 0) {
+            if (score <= 0) {
                 view.animate().translationX(200F).setInterpolator(AccelerateDecelerateInterpolator()).setDuration(150).start()
             } else {
                 view.animate().translationX(0f).setInterpolator(AccelerateDecelerateInterpolator()).setDuration(150).start()
-                if(score == 180){
-                    view.setTextColor(Color.parseColor("#FCC804"))
+                if (score == 180) {
+                    view.setTextColor(view.context.getColor(R.color.colorOneEighty))
                 } else {
-                    view.setTextColor(view.context.getColor(R.color.white))
+                    view.setTextColor(fromAttr(view.context, R.attr.scoreText))
                 }
             }
+        }
+
+        @ColorInt
+        private fun fromAttr(context: Context, attr: Int): Int {
+            val typedValue = TypedValue()
+            val theme = context.theme
+            theme.resolveAttribute(attr, typedValue, true)
+            val res = typedValue.resourceId
+            return context.getColor(res)
         }
 
         @JvmStatic
         @BindingAdapter("finish")
         fun showFinish(view: TextView, finish: String) {
             view.text = finish
-            if(finish.isEmpty()) {
+            if (finish.isEmpty()) {
                 view.animate().translationX(-200F).setInterpolator(AccelerateDecelerateInterpolator()).setDuration(150).start()
             } else {
                 view.animate().translationX(0f).setInterpolator(AccelerateDecelerateInterpolator()).setDuration(150).start()
