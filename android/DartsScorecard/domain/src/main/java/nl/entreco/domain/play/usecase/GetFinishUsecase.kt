2ac241ac585facf58a623ea1a345726ca88dc926
Remco Janssen <entreco@gmail.com>
2017-11-25 11:08:27 +0100

    #3: Adding (part of) Standard finish table

--- android/DartsScorecard/domain/src/main/java/nl/entreco/domain/play/usecase/GetFinishUsecase.kt
+++ android/DartsScorecard/domain/src/main/java/nl/entreco/domain/play/usecase/GetFinishUsecase.kt
@@ -37,11 +37,49 @@ open class GetFinishUsecase @Inject constructor() {
 
         // Now, hardcode some values that do not need favourite Double, since
         // it can only be done in 1 way
-        if (score == 170 && turn.dartsLeft() >= 3) return "T20 T20 BULL"
-        if (score == 167 && turn.dartsLeft() >= 3) return "T20 T19 BULL"
-        if (score == 164 && turn.dartsLeft() >= 3) return "T20 T18 BULL"
-        if (score == 2 && turn.dartsLeft() >= 1) return "D1"
-        if (score == 3 && turn.dartsLeft() >= 1) return "1 D1"
+        if (require(score, turn, 170, 3)) return "T20 T20 BULL"
+        if (require(score, turn, 167, 3)) return "T20 T19 BULL"
+        if (require(score, turn, 164, 3)) return "T20 T18 BULL"
+        if (require(score, turn, 161, 3)) return "T20 T17 BULL"
+        if (require(score, turn, 160, 3)) return "T20 T20 D20"
+        if (require(score, turn, 158, 3)) return "T20 T20 D19"
+        if (require(score, turn, 157, 3)) return "T20 T19 D20"
+        if (require(score, turn, 156, 3)) return "T20 T20 D18"
+        if (require(score, turn, 155, 3)) return "T20 T19 D19"
+        if (require(score, turn, 154, 3)) return "T20 T18 D20"
+        if (require(score, turn, 153, 3)) return "T20 T19 D18"
+        if (require(score, turn, 152, 3)) return "T20 T20 D16"
+        if (require(score, turn, 151, 3)) return "T20 T17 D20"
+        if (require(score, turn, 150, 3)) return "T20 T18 D18"
+        if (require(score, turn, 149, 3)) return "T20 T19 D16"
+        if (require(score, turn, 148, 3)) return "T20 T16 D20"
+        if (require(score, turn, 147, 3)) return "T19 T18 D18"
+        if (require(score, turn, 146, 3)) return "T20 T18 D16"
+        if (require(score, turn, 145, 3)) return "T20 T15 D20"
+
+
+        if (require(score, turn, 80, 2)) return "T20 D10"
+        if (require(score, turn, 79, 2)) return "T13 D20"
+        if (require(score, turn, 78, 2)) return "T18 D12"
+        if (require(score, turn, 77, 2)) return "T19 D10"
+        if (require(score, turn, 76, 2)) return "T20 D8"
+        if (require(score, turn, 75, 2)) return "T17 D12"
+        if (require(score, turn, 74, 2)) return "T18 D10"
+        if (require(score, turn, 73, 2)) return "T19 D8"
+        if (require(score, turn, 72, 2)) return "T12 D18"
+        if (require(score, turn, 71, 2)) return "T17 D10"
+        if (require(score, turn, 70, 2)) return "T10 D20"
+        if (require(score, turn, 69, 2)) return "T13 D15"
+        if (require(score, turn, 68, 2)) return "T20 D4"
+        if (require(score, turn, 67, 2)) return "T17 D8"
+        if (require(score, turn, 66, 2)) return "T10 D18"
+        if (require(score, turn, 65, 2)) return "S.BULL D20"
+        if (require(score, turn, 64, 2)) return "T16 D8"
+        if (require(score, turn, 63, 2)) return "T13 D12"
+        if (require(score, turn, 62, 2)) return "T10 D16"
+        if (require(score, turn, 61, 2)) return "25 D18"
+        if (require(score, turn, 60, 2)) return "20 D20"
+
 
         // Okay, now we have some options -> so let's go
         if (score == turn.total() && turn.last().isDouble()) return turn.asFinish()
@@ -56,4 +94,7 @@ open class GetFinishUsecase @Inject constructor() {
         // All Doubles, + 1 BULL, All Singles, S.BULL, All Tripples
         return notPossible
     }
+
+    private fun require(score: Int, turn: Turn, target: Int, darts: Int) =
+            score == target && turn.dartsLeft() >= darts
 }
