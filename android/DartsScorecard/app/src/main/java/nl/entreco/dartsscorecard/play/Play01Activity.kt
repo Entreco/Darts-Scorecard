824ecb7876a997078f1b8e203c6fa36da503ad7a
Remco Janssen <entreco@gmail.com>
2017-12-01 23:50:06 +0100

    #5: Initial Stats Experiment for layout/style

--- android/DartsScorecard/app/src/main/java/nl/entreco/dartsscorecard/play/Play01Activity.kt
+++ android/DartsScorecard/app/src/main/java/nl/entreco/dartsscorecard/play/Play01Activity.kt
@@ -2,8 +2,10 @@ package nl.entreco.dartsscorecard.play
 
 import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.support.design.widget.BottomSheetBehavior
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import nl.entreco.dartsscorecard.R
 import nl.entreco.dartsscorecard.base.ViewModelActivity
 import nl.entreco.dartsscorecard.databinding.ActivityPlay01Binding
@@ -24,6 +26,32 @@ class Play01Activity : ViewModelActivity(), ReadyListener {
 
         val binding = DataBindingUtil.setContentView<ActivityPlay01Binding>(this, R.layout.activity_play_01)
         binding.viewModel = viewModel
+
+        val fab = binding.includeInput?.fab!!
+        val inputSheet = binding.includeInput?.inputSheet!!
+        val mainSheet = binding.includeMain?.mainSheet!!
+        val scoreSheet = binding.includeScore?.scoreSheet!!
+
+        val behaviour = BottomSheetBehavior.from(inputSheet)
+        behaviour.setBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+
+                // Slide Out Bottom/Top
+                fab.animate().scaleY(slideOffset).scaleX(slideOffset).setDuration(0).start()
+                scoreSheet.animate().translationY((1 - slideOffset) * -(scoreSheet.height - 100)).setDuration(0).start()
+
+                // Fade Out
+                inputSheet.animate().alpha(slideOffset).setDuration(0).start()
+                scoreSheet.animate().alpha(slideOffset).setDuration(0).start()
+
+                // Fade In MainSheet
+                mainSheet.animate().alpha(1 -slideOffset).setDuration(0).start()
+                mainSheet.animate().translationY((slideOffset) * -(mainSheet.height)).setDuration(0).start()
+            }
+
+            override fun onStateChanged(bottomSheet: View, newState: Int) {}
+        })
+        behaviour.state = BottomSheetBehavior.STATE_EXPANDED
     }
 
     override fun onReady() {
