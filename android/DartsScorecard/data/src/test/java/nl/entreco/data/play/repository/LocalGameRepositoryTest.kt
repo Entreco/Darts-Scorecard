7b4e0764b842d8ef068338754d8a6cb79174b55e
Remco Janssen <entreco@gmail.com>
2017-12-17 00:14:21 +0100

    #5: Test for NonNull parameter in FetchAll

--- android/DartsScorecard/data/src/test/java/nl/entreco/data/play/repository/LocalGameRepositoryTest.kt
+++ android/DartsScorecard/data/src/test/java/nl/entreco/data/play/repository/LocalGameRepositoryTest.kt
@@ -1,9 +1,11 @@
 package nl.entreco.data.play.repository
 
+import com.nhaarman.mockito_kotlin.verify
 import com.nhaarman.mockito_kotlin.whenever
 import nl.entreco.data.DscDatabase
 import nl.entreco.data.db.game.GameDao
 import nl.entreco.data.db.game.GameMapper
+import nl.entreco.data.db.game.GameTable
 import nl.entreco.domain.play.model.Game
 import org.junit.Assert.assertNotNull
 import org.junit.Before
@@ -34,5 +36,43 @@ class LocalGameRepositoryTest {
     fun `it should create a new game`() {
         val game = subject.create("uid", "1|2", 2, 3, 501, 0)
         assertNotNull(game)
+    }
+
+    @Test
+    fun `it should fetch previous games if they exist`() {
+        givenExistingGames()
+        whenFetchingAll()
+        thenFetchAllIsCalledOnDao()
+    }
+
+    @Test(expected = IllegalStateException::class)
+    fun `it should throw exception when no previous games exist`() {
+        givenNoGames()
+        whenFetchingAll()
+        thenFetchAllIsCalledOnDao()
+    }
+
+    private fun thenFetchAllIsCalledOnDao() {
+        verify(mockGameDao).fetchAll()
+    }
+
+    private fun whenFetchingAll() {
+        subject.fetchLatest()
+    }
+
+    private fun givenExistingGames(){
+        val table = GameTable()
+        table.startIndex = 0
+        table.startScore = 5
+        table.numSets = 5
+        table.numLegs = 5
+        table.teams = "1,2|3"
+        table.uid = "some uid"
+        val games = listOf(table)
+        whenever(mockGameDao.fetchAll()).thenReturn(games)
+    }
+
+    private fun givenNoGames(){
+        whenever(mockGameDao.fetchAll()).thenReturn(emptyList())
     }
 }
