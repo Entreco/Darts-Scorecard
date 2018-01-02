7ad07683068c5d5f76764c47992a430f6557faa6
Remco Janssen <entreco@gmail.com>
2018-01-02 23:58:53 +0100

    #4: Testing OutOfBounds on Selecting

--- android/DartsScorecard/app/src/test/java/nl/entreco/dartsscorecard/setup/players/PlayerViewModelTest.kt
+++ android/DartsScorecard/app/src/test/java/nl/entreco/dartsscorecard/setup/players/PlayerViewModelTest.kt
@@ -1,6 +1,7 @@
 package nl.entreco.dartsscorecard.setup.players
 
 import android.widget.AdapterView
+import com.nhaarman.mockito_kotlin.any
 import com.nhaarman.mockito_kotlin.whenever
 import org.junit.Assert.assertEquals
 import org.junit.Test
@@ -14,7 +15,7 @@ import org.mockito.junit.MockitoJUnitRunner
 @RunWith(MockitoJUnitRunner::class)
 class PlayerViewModelTest {
 
-    @Mock private lateinit var mockAdapter : AdapterView<*>
+    @Mock private lateinit var mockAdapter: AdapterView<*>
     private lateinit var subject: PlayerViewModel
 
     @Test
@@ -36,6 +37,19 @@ class PlayerViewModelTest {
         thenTeamIndexIs(4)
     }
 
+    @Test
+    fun `it should update teamIndex with default when Spinner item selected is out of range`() {
+        givenSubject(1)
+        whenInvalidTeamIndexSelected(4)
+        thenTeamIndexIs(1)
+    }
+
+    @Test(expected = Exception::class)
+    fun `it should throw if other exceptions occur`() {
+        givenSubject(1)
+        whenWeirdErrorOccurs()
+    }
+
     private fun givenSubject(index: Int) {
         subject = PlayerViewModel(index)
     }
@@ -43,6 +57,16 @@ class PlayerViewModelTest {
     private fun whenTeamIndexSelected(expectedIndex: Int) {
         whenever(mockAdapter.getItemAtPosition(expectedIndex)).thenReturn(expectedIndex)
         subject.onTeamSelected(mockAdapter, expectedIndex)
+    }
+
+    private fun whenInvalidTeamIndexSelected(expectedIndex: Int) {
+        whenever(mockAdapter.getItemAtPosition(expectedIndex)).thenThrow(IndexOutOfBoundsException())
+        subject.onTeamSelected(mockAdapter, expectedIndex)
+    }
+
+    private fun whenWeirdErrorOccurs() {
+        whenever(mockAdapter.getItemAtPosition(any())).thenReturn(NullPointerException())
+        subject.onTeamSelected(mockAdapter, 600)
     }
 
     private fun thenPlayerNameIs(expectedName: String) {
