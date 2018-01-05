4ec172d1acd28952f1eddbfa865302596fb6fe75
Remco Janssen <Entreco@gmail.com>
2018-01-05 21:02:11 +0100

    #4: Various tweaks for Setup Player

--- android/DartsScorecard/app/src/main/java/nl/entreco/dartsscorecard/setup/edit/EditPlayerActivity.kt
+++ android/DartsScorecard/app/src/main/java/nl/entreco/dartsscorecard/setup/edit/EditPlayerActivity.kt
@@ -2,6 +2,7 @@ package nl.entreco.dartsscorecard.setup.edit
 
 import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.support.v7.widget.Toolbar
 import nl.entreco.dartsscorecard.R
 import nl.entreco.dartsscorecard.base.ViewModelActivity
 import nl.entreco.dartsscorecard.databinding.ActivityEditPlayerBinding
@@ -15,23 +16,22 @@ class EditPlayerActivity : ViewModelActivity() {
 
     private val component: EditPlayerComponent by componentProvider { it.plus(EditPlayerModule(getSuggestedName())) }
     private val viewModel: EditPlayerViewModel by viewModelProvider { component.viewModel() }
-    private val navigator: EditPlayerNavigator by lazy{ EditPlayerNavigator(this)}
+    private val navigator: EditPlayerNavigator by lazy { EditPlayerNavigator(this) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = DataBindingUtil.setContentView<ActivityEditPlayerBinding>(this, R.layout.activity_edit_player)
         binding.viewModel = viewModel
         binding.navigator = navigator
-        initToolbar()
-    }
-
-    private fun initToolbar() {
-        setTitle(R.string.title_select_player)
-        supportActionBar?.setDisplayShowHomeEnabled(true)
+        initToolbar(toolbar(binding), R.string.title_select_player)
     }
 
     private fun getSuggestedName(): String {
         return intent.getStringExtra("suggestion")
+    }
+
+    private fun toolbar(binding: ActivityEditPlayerBinding): Toolbar {
+        return binding.includeToolbar?.toolbar!!
     }
 
     override fun onBackPressed() {
--- android/DartsScorecard/app/src/main/java/nl/entreco/dartsscorecard/setup/edit/EditPlayerViewModel.kt
+++ android/DartsScorecard/app/src/main/java/nl/entreco/dartsscorecard/setup/edit/EditPlayerViewModel.kt
@@ -81,7 +81,7 @@ class EditPlayerViewModel @Inject constructor(private val createPlayerUsecase: C
 
     fun onActionDone(view: TextView, action: Int, navigator: EditPlayerNavigator): Boolean {
         if (action == EditorInfo.IME_ACTION_DONE) {
-            val existing = allPlayers.findLast { it.name.toLowerCase() == view.text.toString() }
+            val existing = allPlayers.findLast { it.name.toLowerCase() == view.text.toString().toLowerCase() }
             if (existing == null) {
                 createPlayerUsecase.exec(CreatePlayerRequest(view.text.toString(), 16),
                         onCreateSuccess(navigator),
