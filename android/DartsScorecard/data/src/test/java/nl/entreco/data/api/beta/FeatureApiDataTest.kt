b5ae61cb0622ee851778a18e9850c92d13fb622e
Remco Janssen <Entreco@gmail.com>
2018-02-20 09:16:28 +0100

    #41: Fix FeatureApiDataTest

--- android/DartsScorecard/data/src/test/java/nl/entreco/data/api/beta/FeatureApiDataTest.kt
+++ android/DartsScorecard/data/src/test/java/nl/entreco/data/api/beta/FeatureApiDataTest.kt
@@ -7,41 +7,36 @@ import org.junit.Test
  * Created by entreco on 06/02/2018.
  */
 class FeatureApiDataTest {
-    private val description = "description"
-    private val image = "some.image/com"
-    private val title = "Some title"
-    private val goal = 100
-    private val count = 50
-    private val subject = FeatureApiData(description, image, title, "", goal, count)
+    private val subject = FeatureApiData()
 
     @Test
     fun `description is empty initially`() {
-        assertEquals(description, subject.description)
+        assertEquals("", subject.description)
     }
 
     @Test
     fun `image is empty initially`() {
-        assertEquals(image, subject.image)
+        assertEquals("", subject.image)
     }
 
     @Test
     fun `title is empty initially`() {
-        assertEquals(title, subject.title)
+        assertEquals("", subject.title)
     }
 
     @Test
-    fun `feature is empty initially`() {
-        assertEquals("", subject.remco)
+    fun `progress is empty initially`() {
+        assertEquals("", subject.progress)
     }
 
     @Test
     fun `goal is zero initially`() {
-        assertEquals(goal, subject.goal)
+        assertEquals(0, subject.goal)
     }
 
     @Test
     fun `count is zero initially`() {
-        assertEquals(count, subject.count)
+        assertEquals(0, subject.count)
     }
 
 }
