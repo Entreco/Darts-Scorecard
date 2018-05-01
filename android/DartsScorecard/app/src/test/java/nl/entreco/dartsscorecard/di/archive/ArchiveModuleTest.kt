eb90d8757f211db08d69a1a97a50161b8f641d68
Remco Janssen <entreco@gmail.com>
2018-05-01 23:20:08 +0200

    #30: Update ModuleTest with db and logger

--- android/DartsScorecard/app/src/test/java/nl/entreco/dartsscorecard/di/archive/ArchiveModuleTest.kt
+++ android/DartsScorecard/app/src/test/java/nl/entreco/dartsscorecard/di/archive/ArchiveModuleTest.kt
@@ -1,13 +1,21 @@
 package nl.entreco.dartsscorecard.di.archive
 
+import nl.entreco.data.db.DscDatabase
+import nl.entreco.domain.common.log.Logger
+import org.junit.Assert.assertNotNull
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
 
-import org.junit.Assert.*
-
+@RunWith(MockitoJUnitRunner::class)
 class ArchiveModuleTest {
+
+    @Mock private lateinit var mockDb: DscDatabase
+    @Mock private lateinit var mockLogger: Logger
 
     @Test
     fun provideArchiveRepository() {
-        assertNotNull(ArchiveModule().provideArchiveRepository())
+        assertNotNull(ArchiveModule().provideArchiveRepository(mockDb, mockLogger))
     }
 }
