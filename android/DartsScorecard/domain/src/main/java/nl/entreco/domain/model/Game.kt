3304992ec99f8fa0e820039901484d1b8166c217
Remco Janssen <entreco@gmail.com>
2018-05-01 23:18:37 +0200

    #30: Game Match.START change

--- android/DartsScorecard/domain/src/main/java/nl/entreco/domain/model/Game.kt
+++ android/DartsScorecard/domain/src/main/java/nl/entreco/domain/model/Game.kt
@@ -17,7 +17,7 @@ data class Game(val id: Long = 0, val arbiter: Arbiter) {
     fun start(startIndex: Int, teams: Array<Team>): Game {
         next = arbiter.start(startIndex, teams)
         starters.clear()
-        starters.add( 0, next.team)
+        starters.add(0, next.team)
         previousState = next.state
         newMatchSetOrLeg = true
         return this
@@ -32,9 +32,10 @@ data class Game(val id: Long = 0, val arbiter: Arbiter) {
     }
 
     private fun updateStartTeam(next: Next) {
+        if (previousState == State.START) return
         newMatchSetOrLeg = when (next.state) {
             State.MATCH, State.LEG, State.SET -> {
-                starters.add( 0, next.team)
+                starters.add(0, next.team)
                 true
             }
             else -> {
@@ -56,7 +57,7 @@ data class Game(val id: Long = 0, val arbiter: Arbiter) {
     }
 
     fun wasBreakMade(by: Player): Boolean {
-        if(starters.size < 2) return false
+        if (starters.size < 2) return false
         return newMatchSetOrLeg && !starters[1].contains(by)
     }
 }
