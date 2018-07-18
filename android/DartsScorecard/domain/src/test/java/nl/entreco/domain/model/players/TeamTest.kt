ce3e32d155161eb0a82d6131dced5eec93f948f4
Remco Janssen <entreco@gmail.com>
2018-07-18 10:16:39 +0200

    #49: Fix TeamImage name Test

--- android/DartsScorecard/domain/src/test/java/nl/entreco/domain/model/players/TeamTest.kt
+++ android/DartsScorecard/domain/src/test/java/nl/entreco/domain/model/players/TeamTest.kt
@@ -9,13 +9,13 @@ import org.mockito.junit.MockitoJUnitRunner
  * Created by Entreco on 18/11/2017.
  */
 @RunWith(MockitoJUnitRunner::class)
-class TeamTest{
+class TeamTest {
 
     private val player1 = Player("one")
     private val player2 = Player("two")
     private val player3 = Player("three")
 
-    val subject : Team = Team(arrayOf(player1, player2))
+    val subject: Team = Team(arrayOf(player1, player2))
 
     @Test
     fun `it should have non null list of players`() {
@@ -53,8 +53,8 @@ class TeamTest{
     }
 
     @Test
-    fun `it should NOT have image initially`() {
-        assertNull(subject.imageUrl())
+    fun `it should have image initially`() {
+        assertEquals("team2", subject.imageUrl())
     }
 
     @Test
