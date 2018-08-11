23a39c0b9675aa7336316927acc940e56189632a
Remco Janssen <entreco@gmail.com>
2018-08-11 16:11:21 +0200

    #14: Fix UnitTest for ServiceModule

--- android/DartsScorecard/app/src/test/java/nl/entreco/dartsscorecard/di/service/ServiceModuleTest.kt
+++ android/DartsScorecard/app/src/test/java/nl/entreco/dartsscorecard/di/service/ServiceModuleTest.kt
@@ -1,12 +1,19 @@
 package nl.entreco.dartsscorecard.di.service
 
+import android.app.Service
 import org.junit.Assert.*
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
 
+@RunWith(MockitoJUnitRunner::class)
 class ServiceModuleTest{
+
+    @Mock private lateinit var mockService: Service
 
     @Test
     fun `can be created`() {
-        assertNotNull(ServiceModule())
+        assertNotNull(ServiceModule(mockService))
     }
 }
