fec9111d103c5e299143985a83da6f08f90dd63e
Remco Janssen <entreco@gmail.com>
2019-06-10 09:52:17 +0200

    #54: Cleanup & Localised Settings screen

--- android/DartsScorecard/app/src/main/java/nl/entreco/dartsscorecard/base/widget/WrapContentViewPager.kt
+++ android/DartsScorecard/app/src/main/java/nl/entreco/dartsscorecard/base/widget/WrapContentViewPager.kt
@@ -1,18 +1,17 @@
 package nl.entreco.dartsscorecard.base.widget
 
 import android.content.Context
-import androidx.databinding.Bindable
-import androidx.viewpager.widget.ViewPager
 import android.util.AttributeSet
+import androidx.viewpager.widget.ViewPager
 
 /**
  * Created by entreco on 24/03/2018.
  */
-class WrapContentViewPager(context: Context, attrs: AttributeSet?) : androidx.viewpager.widget.ViewPager(context, attrs) {
+class WrapContentViewPager(context: Context, attrs: AttributeSet?) : ViewPager(context, attrs) {
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        val hms = getChildAt(0)?.let {child ->
+        val hms = getChildAt(0)?.let { child ->
             child.measure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED))
-                MeasureSpec.makeMeasureSpec(child.measuredHeight, MeasureSpec.EXACTLY)
+            MeasureSpec.makeMeasureSpec(child.measuredHeight, MeasureSpec.EXACTLY)
         }
         super.onMeasure(widthMeasureSpec, hms ?: heightMeasureSpec)
     }
