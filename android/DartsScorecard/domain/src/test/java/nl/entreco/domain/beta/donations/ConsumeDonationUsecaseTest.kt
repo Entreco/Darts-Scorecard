4a9b18b44baca4d17777d3fe3a7116ac7347f503
Remco Janssen <entreco@gmail.com>
2019-10-06 11:38:16 +0200

    #63: Update UnitTests

--- android/DartsScorecard/domain/src/test/java/nl/entreco/domain/beta/donations/ConsumeDonationUsecaseTest.kt
+++ android/DartsScorecard/domain/src/test/java/nl/entreco/domain/beta/donations/ConsumeDonationUsecaseTest.kt
@@ -1,6 +1,9 @@
 package nl.entreco.domain.beta.donations
 
 import com.nhaarman.mockito_kotlin.any
+import com.nhaarman.mockito_kotlin.argumentCaptor
+import com.nhaarman.mockito_kotlin.doThrow
+import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.verify
 import com.nhaarman.mockito_kotlin.verifyZeroInteractions
 import com.nhaarman.mockito_kotlin.whenever
@@ -16,22 +19,17 @@ import org.mockito.junit.MockitoJUnitRunner
 /**
  * Created by entreco on 18/02/2018.
  */
-@RunWith(MockitoJUnitRunner::class)
 class ConsumeDonationUsecaseTest {
 
     private val bg = TestBackground()
     private val fg = TestForeground()
 
-    @Mock private lateinit var mockBillingRepository: BillingRepository
-    @Mock private lateinit var done: (ConsumeDonationResponse) -> Unit
-    @Mock private lateinit var fail: (Throwable) -> Unit
+    private val mockBillingRepository: BillingRepository = mock()
+    private val done: (ConsumeDonationResponse) -> Unit = mock()
+    private val fail: (Throwable) -> Unit = mock()
     private lateinit var subject: ConsumeDonationUsecase
 
-    private val purchaseData = "{  \n" +
-            "   \"purchaseToken\":\"aha\",\n" +
-            "   \"orderId\":\"some order id\",\n" +
-            "   \"sku\":\"id\"\n" +
-            "}"
+    private val purchaseData = "some token"
 
     @Before
     fun setUp() {
@@ -40,33 +38,35 @@ class ConsumeDonationUsecaseTest {
 
     @Test
     fun `it should notify callback of success`() {
-        whenever(mockBillingRepository.consume(any())).thenReturn(1)
-        subject.exec(ConsumeDonationRequest(purchaseData, "sig", true), done, fail)
+        val doneCaptor = argumentCaptor<(MakePurchaseResponse)->Unit>()
+        subject.exec(ConsumeDonationRequest(purchaseData, "sku", "orderid", true), done, fail)
+        verify(mockBillingRepository.consume(any(), doneCaptor.capture()))
+        doneCaptor.lastValue.invoke(MakePurchaseResponse.Consumed)
         verify(done).invoke(any())
     }
 
     @Test
     fun `it should notify callback of failure`() {
-        whenever(mockBillingRepository.consume(any())).thenThrow(RuntimeException("Oh no you didn't"))
-        subject.exec(ConsumeDonationRequest(purchaseData, "sig", true), done, fail)
+        whenever(mockBillingRepository.consume(any(), any())).doThrow(RuntimeException("Oh no you didn't"))
+        subject.exec(ConsumeDonationRequest(purchaseData, "sku", "orderid", true), done, fail)
         verify(fail).invoke(any())
     }
 
     @Test
     fun `it should notify callback of failure when invalid json object`() {
-        subject.exec(ConsumeDonationRequest("thiz iz no json dude", "sig", true), done, fail)
+        subject.exec(ConsumeDonationRequest("thiz iz no json dude", "sku", "id", true), done, fail)
         verify(fail).invoke(any())
     }
 
     @Test
     fun `it should not consume item when 'requiresConsumption is false'`() {
-        subject.exec(ConsumeDonationRequest(purchaseData, "sig", false), done, fail)
+        subject.exec(ConsumeDonationRequest(purchaseData, "sku", "id", false), done, fail)
         verifyZeroInteractions(mockBillingRepository)
     }
 
     @Test
     fun `it should notify success when 'requiresConsumption is false'`() {
-        subject.exec(ConsumeDonationRequest(purchaseData, "sig", false), done, fail)
+        subject.exec(ConsumeDonationRequest(purchaseData, "sku", "id", false), done, fail)
         verify(done).invoke(any())
     }
 
